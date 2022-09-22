58a2a59395657bd87120f3f2be89e4c08cf15b12
Eliott Raheriarisoa <eliott.raheriarisoa@gmail.com>
2022-09-22 12:27:33 +0200

    test

--- src/Pages/Login/Styled.jsx
+++ src/Pages/Login/Styled.jsx
@@ -3,9 +3,9 @@ import styled from "styled-components"
 export const MainContainer = styled.div`
   width: 100vw;
   height: 100vh;
+  max-height: 100vh;
   background-color: white;
   position: absolute;
   top: 0;
   overflow: hidden;
-  scroll-behavior: none;
 `
